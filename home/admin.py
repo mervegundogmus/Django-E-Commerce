c027641fc4dde2fdd353e276d351d6d5736ed35f
mervegundogmus <mervegundogmus@outlook.com>
2020-08-22 02:45:16 +0300

    Üye Profil ile Ek Bilgiler ve Resim

--- home/admin.py
+++ home/admin.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
-
+from home.models import UserProfile
 # Register your models here.
 from home.models import Setting
 
+class UserProfileAdmin(admin.ModelAdmin):
+    list_display = ['user_name','phone','address','city','country','image_tag']
 
 admin.site.register(Setting)
+admin.site.register(UserProfile, UserProfileAdmin)
 
--- home/models.py
+++ home/models.py
@@ -1,6 +1,8 @@
-
+from django.contrib.auth.models import User
 from django.db import models
 from ckeditor_uploader.fields import RichTextUploadingField
+from django.forms import ModelForm
+from django.utils.safestring import mark_safe
 # Create your models here.
 
 class Setting(models.Model):
@@ -32,4 +34,28 @@ class Setting(models.Model):
 
     def __str__(self):
         return self.title
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    phone = models.CharField(max_length=20)
+    address = models.CharField(max_length=150)
+    city = models.CharField(max_length=20)
+    country = models.CharField(max_length=20)
+    image = models.ImageField(blank=True, upload_to='images/users/')
+
+    def __str__(self):
+        return self.user.username
+
+    def user_name(self):
+        return '['+self.user.username + '] ' + self.user.first_name +' '+ self.user.last_name
+
+    def image_tag(self):
+        return mark_safe('<img src="{}" height="50"/>'.format(self.image.url))
+
+    image_tag.short_description='Image'
+
+class UserProfileForm(ModelForm):
+    class Meta:
+        model=UserProfile
+        fields = ['phone','address','city','country','image']
 
